78a40cce6bb7c33f7cb44b396e1fae4d49b96977
Adam Pod <adam.podoxin@gmail.com>
2019-01-19 17:25:42 -0800

    Make mouse look

--- Dark/Assets/Scripts/Player/PlayerMovement.cs
+++ Dark/Assets/Scripts/Player/PlayerMovement.cs
@@ -5,18 +5,25 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public float mouseSensitivity = 3f;
 
     private Rigidbody rb;
     private Vector3 movement;
 
+    private new Transform camera;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        camera = Camera.main.transform;
     }
 
     private void FixedUpdate()
     {
         movement = new Vector3(Input.GetAxisRaw("Horizontal"), rb.velocity.y, Input.GetAxisRaw("Vertical")) * moveSpeed;
         rb.velocity = movement;
+
+        transform.eulerAngles += Vector3.up * Input.GetAxisRaw("Mouse X") * mouseSensitivity;
+        camera.eulerAngles += new Vector3(-Input.GetAxisRaw("Mouse Y"), Input.GetAxisRaw("Mouse X")) * mouseSensitivity;
     }
 }
