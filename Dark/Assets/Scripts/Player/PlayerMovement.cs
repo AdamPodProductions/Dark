b99a5583a29d8ab8405ce64c94dd90581cdef252
Adam Pod <adam.podoxin@gmail.com>
2019-01-19 17:22:07 -0800

    Make basic player movement

---
+++ Dark/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    private Rigidbody rb;
+    private Vector3 movement;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        movement = new Vector3(Input.GetAxisRaw("Horizontal"), rb.velocity.y, Input.GetAxisRaw("Vertical")) * moveSpeed;
+        rb.velocity = movement;
+    }
+}
