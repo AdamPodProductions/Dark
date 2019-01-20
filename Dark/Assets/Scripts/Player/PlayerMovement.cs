c614045bea68713fb6f70ab38c42224753df983d
Adam Pod <adam.podoxin@gmail.com>
2019-01-19 17:26:41 -0800

    Make seperate functions for movement and mouse look
    Functions are named "Movement" and "MouseLook".

--- Dark/Assets/Scripts/Player/PlayerMovement.cs
+++ Dark/Assets/Scripts/Player/PlayerMovement.cs
@@ -20,9 +20,18 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        Movement();
+        MouseLook();
+    }
+
+    private void Movement()
+    {
         movement = new Vector3(Input.GetAxisRaw("Horizontal"), rb.velocity.y, Input.GetAxisRaw("Vertical")) * moveSpeed;
         rb.velocity = movement;
+    }
 
+    private void MouseLook()
+    {
         transform.eulerAngles += Vector3.up * Input.GetAxisRaw("Mouse X") * mouseSensitivity;
         camera.eulerAngles += new Vector3(-Input.GetAxisRaw("Mouse Y"), Input.GetAxisRaw("Mouse X")) * mouseSensitivity;
     }
