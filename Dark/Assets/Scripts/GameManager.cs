4a7f47cc007ebc3ad1b3abf3d34cec8b013d96cf
Adam Pod <adam.podoxin@gmail.com>
2019-01-20 20:56:55 -0800

    Implement radar display toggle

--- Dark/Assets/Scripts/GameManager.cs
+++ Dark/Assets/Scripts/GameManager.cs
@@ -4,9 +4,26 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private PlayerMovement playerMovement;
+    private GameObject radarDisplay;
+
     private void Start()
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+
+        playerMovement = FindObjectOfType<PlayerMovement>();
+        radarDisplay = FindObjectOfType<RadarDisplay>().gameObject;
+
+        radarDisplay.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            radarDisplay.SetActive(!radarDisplay.activeInHierarchy);
+            playerMovement.CanMove = !playerMovement.CanMove;
+        }
     }
 }
