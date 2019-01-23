13411407f34712476dfca7dcd1beb2f673d0d8fb
Adam Pod <adam.podoxin@gmail.com>
2019-01-22 16:16:34 -0800

    Create and add scene changer

---
+++ Dark/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public GameObject loading;
+
+    public void ChangeScene(int sceneIndex)
+    {
+        loading.SetActive(true);
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void ChangeScene(string sceneName)
+    {
+        loading.SetActive(true);
+        SceneManager.LoadScene(sceneName);
+    }
+}
