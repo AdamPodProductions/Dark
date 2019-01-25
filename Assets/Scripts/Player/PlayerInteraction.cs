b41e2abd66b56beda797a9ed0dac3b48a00ebdc2
Adam Pod <adam.podoxin@gmail.com>
2019-01-24 16:27:11 -0800

    Create player interaction system

---
+++ Assets/Scripts/Player/PlayerInteraction.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    public Transform rayPoint;
+
+    private void Update()
+    {
+        CheckRaycast();
+    }
+
+    private void CheckRaycast()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(rayPoint.position, rayPoint.forward, out hit, 4.5f))
+        {
+            if (hit.transform.CompareTag("Gas"))
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    print("Activate gas");
+                }
+            }
+        }
+    }
+}
