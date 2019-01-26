5f2714f237a77de95cbcdfeaa0621a834d0a569e
Adam Pod <adam.podoxin@gmail.com>
2019-01-26 12:16:52 -0800

    Remove sprint

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,20 +7,11 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
 
-    [SerializeField]
-    private float stamina = 4f;
-    public float Stamina { get { return stamina; } set { stamina = value; } }
-
     [Range(1, 10)]
     public float mouseSensitivity = 3f;
 
     private bool canMove = true;
     public bool CanMove { get { return canMove; } set { canMove = value; } }
-
-    [Space]
-
-    public GameObject isSprintingImage;
-    public Image staminaBar;
 
     [SerializeField]
     private RadarDisplay radarDisplay;
@@ -32,15 +23,11 @@ public class PlayerMovement : MonoBehaviour
     private AudioSource audioSource;
     private float footstepTimer = 0f;
 
-    private float speedMultiplier = 1f;
-    private bool sprinting = false;
     private Vector3 movement;
 
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
-
-        speedMultiplier = moveSpeed;
 
         radarDisplay.AddBlip(transform, Color.green);
     }
@@ -49,58 +36,16 @@ public class PlayerMovement : MonoBehaviour
     {
         if (CanMove)
         {
-            CheckSprint();
             Movement();
-
             MouseLook();
-
             CheckFootstep();
         }
     }
 
-    private void ReplenishStamina()
-    {
-        if (Stamina < 4f)
-        {
-            Stamina += Time.deltaTime;
-        }
-    }
-
-    private void DrainStamina()
-    {
-        Stamina -= Time.deltaTime;
-    }
-
     private void Movement()
     {
-        movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")) * speedMultiplier * moveSpeed * Time.deltaTime;
+        movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")) * moveSpeed * Time.deltaTime;
         transform.Translate(movement);
-    }
-
-    private void CheckSprint()
-    {
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            sprinting = !sprinting;
-            isSprintingImage.SetActive(sprinting);
-        }
-
-        if (sprinting)
-        {
-            if (Stamina > 0f && !movement.Equals(Vector3.zero))
-            {
-                speedMultiplier = 2f;
-                DrainStamina();
-            }
-        }
-        else
-        {
-            speedMultiplier = 1f;
-            sprinting = false;
-            ReplenishStamina();
-        }
-
-        staminaBar.fillAmount = Stamina / 4f;
     }
 
     private void MouseLook()
@@ -118,12 +63,12 @@ public class PlayerMovement : MonoBehaviour
     {
         if (!movement.Equals(Vector3.zero))
         {
-            footstepTimer -= Time.deltaTime * speedMultiplier;
+            footstepTimer -= Time.deltaTime;
 
             if (footstepTimer < 0)
             {
                 PlayFootstepSound();
-                footstepTimer = 0.5f;
+                footstepTimer = 0.4f;
             }
         }
     }
